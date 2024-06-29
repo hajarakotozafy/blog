282e7e5dd854a3e2ce47b5c99178d3ed15998fac
Haja <hajarakotozafy45@gmail.com>
2024-06-29 19:03:26 +0300

    fix:change role switcher texts

--- src/components/RoleSwitcher/RoleSwitcher.tsx
+++ src/components/RoleSwitcher/RoleSwitcher.tsx
@@ -6,11 +6,11 @@ const RoleSwitcher = ({isAdmin}: boolean) => {
 			<form>
 				<div className="role-switcher-form-control">
 				<input type="radio" name="role" id="admin" value="admin" checked/>
-				<label for="admin">Admin</label>
+				<label for="admin">Admin view</label>
 				</div>
 				<div className="role-switcher-form-control">
 				<input type="radio" name="role" id="user" value="user"/>
-				<label for="user">User</label>
+				<label for="user">User view</label>
 				</div>
 			</form>
 		</div>
